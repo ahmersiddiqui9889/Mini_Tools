cd5846ba234b8f72eec3b131676f9764a45b24fa
Silky1099 <93541460+Silky1099@users.noreply.github.com>
2022-10-05 20:49:40 +0530

    Create Arithmetic-Calculator.c

---
+++ Tools/Calculator/Arithmetic-Calculator.c
@@ -0,0 +1,33 @@
+/*
+This program can be used to perform the most basic arithmetic operations (addition, subtraction, multiplication, division) on two digits
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    float a,b;
+    char o;
+    clrscr();
+    printf("Enter the operator(+,-,*,/): ");
+    scanf("%c",&o);
+    printf("Enter the first and second numbers: ");
+    scanf("%f %f",&a,&b);
+    switch (o){
+        case '+':
+            printf("%f",a + b);
+            break;
+        case '-':
+            printf("%f",a - b);
+            break;
+        case '*':
+            printf("%f",a * b);
+            break;
+        case '/':
+            printf("%f",a / b);
+            break;
+        default:
+            printf("You entered a wrong operator!");
+    }
+    getch();
+    return 0;
+}
